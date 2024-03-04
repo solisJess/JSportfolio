e3c3d336d22336327ecb899e1fbf3755e596cb36
Jess Solis <103132288+solisJess@users.noreply.github.com>
2024-03-04 16:00:57 -0600

    Update bandname.py

--- Python_Projects/bandname.py
+++ Python_Projects/bandname.py
@@ -1,5 +1,9 @@
-## Write a program with a welcome message that takes as input the name of the city you were born and the name of your pet. 
-## The program should output the two together. 
-## The cursor should output on a new line. 
-## This program tests printing, variables, string manipulation, debugging
+#Write a program with a welcome message that takes as input the name of the city you were born and the name of your pet. 
+# The program should output the two together. 
+# The cursor should output on a new line. 
+# This program tests printing, variables, string manipulation, debugging
  
+print("Welcome to the Band Name Generator!\n")
+city = input('What city were you born in?\n')
+pet_name = input("What is your pet's name?\n")
+print("Your band name is " + city + " " + pet_name)

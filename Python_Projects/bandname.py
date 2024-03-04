0f3ed5b86acbd2518d675b05fed7bf5400ba7cf7
Jess Solis <103132288+solisJess@users.noreply.github.com>
2024-03-04 15:48:15 -0600

    Update bandname.py

--- Python_Projects/bandname.py
+++ Python_Projects/bandname.py
@@ -1,1 +1,5 @@
-
+## Write a program with a welcome message that takes as input the name of the city you were born and the name of your pet. 
+## The program should output the two together. 
+## The cursor should output on a new line. 
+## This program tests printing, variables, string manipulation, debugging
+ 
